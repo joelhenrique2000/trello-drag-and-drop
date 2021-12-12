db3fe4f1267e87dadf67710706a7a27708971e18
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-12-11 23:32:53 -0300

    Adicionado as colunas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 import { AddNewItem } from "./AddNewItem";
+import { Card } from "./Card";
+import { Column } from "./Column";
 import { AppContainer } from "./styles";
 
 export const App: FC = ({ children }) => {
   return (
     <AppContainer>
-      <AddNewItem 
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={console.log}
       />
